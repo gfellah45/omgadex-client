fa09b506a72a205768ffd5773e105da801703d9f
Jerry Aaron <jerry.aaron45@gmail.com>
2021-11-19 11:05:53 +0100

    added confiuration, custom themes and assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3772FF",
+        secondary: "#23262F",
+        badge2: "#FF6838",
+        badge1: "#58BD7D",
+        gray: "#F4F5F6",
+        links: "#777E91",
+        heading: "#23262F",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        sans: ["DM Sans", "sans-seri"],
+      },
+      shadows: {
+        card: "0px 64px 64px -48px rgba(15, 15, 15, 0.1)",
+      },
+    },
   },
   variants: {
     extend: {},
